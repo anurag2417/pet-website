3bfff8c042f913ff9cb1d50777072fa980b03799
Anurag Kumar <anuragakn18@gmail.com>
2026-02-24 15:22:59 +0530

    added cotegories

---
+++ src/data/categories.ts
@@ -0,0 +1,34 @@
+// data/categories.ts
+export const categories = [
+  {
+    id: 'dogs',
+    name: 'Dogs',
+    description: 'From tiny Chihuahuas to majestic Great Danes, find everything about man\'s best friend.',
+    image: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500',
+    path: '/categories/dogs'
+  },
+  {
+    id: 'cats',
+    name: 'Cats',
+    description: 'Independent, curious, and loving - discover the world of feline companions.',
+    image: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=500',
+    path: '/categories/cats'
+  },
+  {
+    id: 'small-mammals',
+    name: 'Small Mammals',
+    description: 'Rabbits, guinea pigs, hamsters, and more - caring for pocket-sized pets.',
+    image: 'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?w=500',
+    path: '/categories/small-mammals'
+  },
+  {
+    id: 'birds',
+    name: 'Birds',
+    description: 'Feathered friends from tiny finches to talking parrots.',
+    image: 'https://images.unsplash.com/photo-1444464666168-49d633b86797?w=500',
+    path: '/categories/birds'
+  },
+  {
+    id: 'reptiles',
+    name: 'Reptiles & Amphibians',
+    description: '
